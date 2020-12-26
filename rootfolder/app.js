8131c0ce4fa9944e9f316c06fd213d8b9ba4b189
parth-commits <parthpatel1087@gmail.com>
2020-12-26 17:34:10 -0500

    website in working condition, will upload now!

--- rootfolder/app.js
+++ rootfolder/app.js
@@ -1,10 +1,10 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
-
+/*
 tl.to('.text-hide', {y: "0%", duration: 1, stagger: 0.25});
 tl.to('.slider', {y:"-150%", duration: 1.5, delay: 0.5});
-tl.to('.intro', {y:"-150%", duration:1}, "-=1");
+tl.to('.intro', {y:"-150%", duration:1}, "-=1");*/
 tl.fromTo('nav', {opacity:0}, {opacity:1, duration: 1});
-tl.fromTo('.hello-im-parth', {opacity:0}, {opacity:1, duration: 1}, "-=1");
+tl.fromTo('.hello-im-parth', {opacity:0}, {opacity:1, duration: 1}, "-=0.8");
 const navSlide = () =>{
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
