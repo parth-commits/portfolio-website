9e3dc18b8bcc4e560dc01adcf4694a6b7698158c
parth-commits <parthpatel1087@gmail.com>
2021-08-14 22:29:24 -0400

    added final redirection to new site. this repo will not be further updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 /*
 tl.to('.text-hide', {y: "0%", duration: 1, stagger: 0.25});
@@ -5,38 +6,10 @@ tl.to('.slider', {y:"-150%", duration: 1.5, delay: 0.5});
 tl.to('.intro', {y:"-150%", duration:1}, "-=1");*/
 tl.fromTo('nav', {opacity:0}, {opacity:1, duration: 1});
 tl.fromTo('.hello-im-parth', {opacity:0}, {opacity:1, duration: 1}, "-=0.8");
-const navSlide = () =>{
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
-
-  burger.addEventListener('click',()=>{
-    //Toggle nav
-    nav.classList.toggle('nav-active');
-
-    //Animate links
-    navLinks.forEach((link, index) => {
-      if(link.style.animation){
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-      }
-    });
-
-    //burger animation
-    burger.classList.toggle('toggle');
-  });
-
-
-
-}
-
-navSlide();
 
 
 
 const faders = document.querySelectorAll(".fade-in");
-const sliders = document.querySelectorAll(".slide-in");
 
 const appearOptions = {
   threshold: 0,
@@ -65,21 +38,10 @@ faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider => {
-  appearOnScroll.observe(slider);
-});
 
 
-const cookieContainer = document.querySelector(".cookies");
-const cookieButton = document.querySelector(".button01");
-
-cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("cookieBannerDisplayed", "true");
-});
-
-setTimeout(() => {
-  if (!localStorage.getItem("cookieBannerDisplayed")) {
-    cookieContainer.classList.add("active");
-  }
-}, 1000);
+async function timeout() {
+  await new Promise(r => setTimeout(r, 1000));
+  window.location.href = "https://parth-commits.github.io/";
+}
+timeout();
